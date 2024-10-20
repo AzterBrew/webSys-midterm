3de77eceb961ccd0bf97f96c0f6d434f3700b0e2
AzterBrew <115197249+AzterBrew@users.noreply.github.com>
2024-10-21 01:05:16 +0800

    eme

--- WebSysMidtermReq/index.php
+++ WebSysMidtermReq/index.php
@@ -11,23 +11,22 @@
     <div id="login">
         <form action="login.php" method="post" class="indexform">
             <div class="content">
-            <h1>Midterm Requirement GRP03</h1>
-            <h2>User Login</h2>
-            <?php   //This lets us have different values depending on conditions by '$_GET'ing that result of a condition from a session. 
-                    //<p> class toh btw
-                if (isset($_GET['error'])) {
-                    echo '<p class="error">' . $_GET['error'] . '</p>';
-                }
-            ?>
-            <label>Username:</label>
-            <input type="text" name="uname" placeholder="Username">
-            <label>Password:</label>
-            <input type="password" name="password" placeholder="Password">
-            <div class="btn">
-                <button type="submit">Login</button>
-                <button type="reset">Reset</button>
-            </div>
-
+                <h1>Midterm Requirement GRP03</h1>
+                <h2>User Login</h2>
+                <?php   //This lets us have different values depending on conditions by '$_GET'ing that result of a condition from a session. 
+                        //<p> class toh btw
+                    if (isset($_GET['error'])) {
+                        echo '<p class="error">' . $_GET['error'] . '</p>';
+                    }
+                ?>
+                <label>Username:</label>
+                <input type="text" name="uname" placeholder="Username">
+                <label>Password:</label>
+                <input type="password" name="password" placeholder="Password">
+                <div class="btn">
+                    <button type="submit">Login</button>
+                    <button type="reset">Reset</button>
+                </div>
             </div>
         </form>
         <div class="form-img">
