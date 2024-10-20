c21bac687c820b0a97af49fd0611db3a401ce18a
frssison <frssison@gmail.com>
2024-10-20 23:32:01 +0800

    commit

--- WebSysMidtermReq/index.php
+++ WebSysMidtermReq/index.php
@@ -10,7 +10,8 @@
 <body>
     <div id="login">
         <form action="login.php" method="post" class="indexform">
-            <h1>Group 3 : Midterm Assessment</h1>
+            <div class="content">
+            <h1>Midterm Requirement GRP03</h1>
             <h2>User Login</h2>
             <?php   //This lets us have different values depending on conditions by '$_GET'ing that result of a condition from a session. 
                     //<p> class toh btw
@@ -18,15 +19,20 @@
                     echo '<p class="error">' . $_GET['error'] . '</p>';
                 }
             ?>
-            <label>User Name</label>
-            <input type="text" name="uname" placeholder="User Name">
-            <label>Password</label>
+            <label>Username:</label>
+            <input type="text" name="uname" placeholder="Username">
+            <label>Password:</label>
             <input type="password" name="password" placeholder="Password">
-            <div>
+            <div class="btn">
                 <button type="submit">Login</button>
                 <button type="reset">Reset</button>
             </div>
+
+            </div>
         </form>
+        <div class="form-img">
+            <img src="./WebSysMidtermReq/images/bg-img.png"alt="Background Image">
+        </div>
     </div>
 </body>
 </html>
